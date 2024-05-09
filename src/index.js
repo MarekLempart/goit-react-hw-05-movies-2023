d5f3105bf93d299f1836fa9587b6479bed8a977e
MarekLempart <kama.zakupy@gmail.com>
2024-05-09 15:33:38 +0200

    praca domowa z StyledComponents

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies-2023">
+    <BrowserRouter basename="/goit-react-hw-05-movies-StyledComponents">
       <App />
     </BrowserRouter>
   </React.StrictMode>
